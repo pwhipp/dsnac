9aeb4eeb442c7b53f69b2fd0434465b4553b5af8
pwhipp <paul.whipp@gmail.com>
2014-09-11 12:05:22 +1000

    Added basic theme templates

--- core/settings/includes/common.py
+++ core/settings/includes/common.py
@@ -98,6 +98,7 @@ INSTALLED_APPS = (
     "django.contrib.sitemaps",
     "django.contrib.staticfiles",
     "django.contrib.messages",
+    'theme',
     "mezzanine.boot",
     "mezzanine.conf",
     "mezzanine.core",
