8e5727fea8dec9024fb265d9b05230ba03820733
pwhipp <paul.whipp@gmail.com>
2014-09-11 16:06:40 +1000

    Add page function

--- bookrepo/views.py
+++ bookrepo/views.py
@@ -1,7 +1,20 @@
-from django.http import HttpResponse, HttpResponseNotFound
+from django.http import HttpResponse, Http404
 
 
 def page(request, book_name, page_number):
-    return HttpResponse('<h1>{book_name}</h1><h2>{page_number}</h2>'.format(
-        book_name=book_name,
-        page_number=page_number))
+    """
+    Return the jp2 image corresponding to the specified page
+    :param request: Request object
+    :param book_name: The folder name being used for the book
+    :param page_number: integer; 0 < page_number < 9999
+    :return:
+    """
+    mime_type = 'image/jp2'
+    file_path = get_book_page_image_file_name(book_name, page_number)
+    if file_path:
+        with open(file_path, 'rb') as f:
+            response = HttpResponse(f.read(), mimetype=mime_type)
+            response['Content-Disposition'] = 'inline;filename={0}'.format(os.path.basename(file_path))
+            return response
+    else:
+        raise Http404
