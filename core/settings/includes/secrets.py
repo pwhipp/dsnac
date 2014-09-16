17c78cde1aa1031ad5fc235d1ef09fd3764ea707
pwhipp <paul.whipp@gmail.com>
2014-09-16 14:46:40 +1000

    Removed secrets.py before putting secrets in

--- core/settings/includes/secrets.py
+++
@@ -1,8 +0,0 @@
-# Only put secret stuff here that you don't want in the repo
-# This file must be manually transferred and updated for each installation but it will hopefully
-# almost never change.
-
-SECRET_KEY = "fieGahshah2aingeuph6ohsohyou9Eezo"
-NEVERCACHE_KEY = "Wolaichoo5ieY7eezaing4ya6shu9iec"
-DBPASSWORD = "miem4Vo6"
-GMAIL_PASSWORD = 'Archive1984#'
