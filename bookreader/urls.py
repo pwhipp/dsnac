c0b11fe0d777ef07df1d8910df4086fa26725e9b
pwhipp <paul.whipp@gmail.com>
2014-09-16 12:13:19 +1000

    Some front page improvements (#3) and login required to read (#13)

--- bookreader/urls.py
+++ bookreader/urls.py
@@ -2,10 +2,11 @@ from __future__ import unicode_literals
 
 from django.conf.urls import patterns, url
 from django.views.generic import TemplateView
+from django.contrib.auth.decorators import login_required
 
 import bookreader.views as bv
 
 urlpatterns = patterns(
     '',
-    url("^/(?P<book_identifier>.+)/$", bv.BookReaderView.as_view(), name="bookreader"),
+    url("^/(?P<book_identifier>.+)/$", login_required(bv.BookReaderView.as_view()), name="bookreader"),
     url("^/demo/$", TemplateView.as_view(template_name='bookreader/demo.html'), name="bookreader_demo"))
--- core/settings/includes/common.py
+++ core/settings/includes/common.py
@@ -112,6 +112,7 @@ INSTALLED_APPS = (
     "mezzanine.forms",
     "mezzanine.pages",
     "mezzanine.galleries",
+    "mezzanine.accounts",
     'bookreader',
     'bookrepo')
 
