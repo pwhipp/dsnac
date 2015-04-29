951caf8ee21749b467bc7d9548c8ad34d2075c84
Alexey Dubnyak <adubnyak@gmail.com>
2015-04-29 12:26:01 +0300

    model migration

--- mediabooks/models.py
+++ mediabooks/models.py
@@ -6,7 +6,9 @@ class Video(models.Model):
     is_active = models.BooleanField(default=True)
     link = models.CharField(max_length=255, help_text='for example: https://vimeo.com/47160217')
     description = models.TextField(default=None, null=True, blank=True)
-
+    creator = models.CharField(max_length=255, default=None, null=True, blank=True)
+    recorded = models.IntegerField(default=None, null=True, blank=True)
+    duration = models.IntegerField(default=None, null=True, blank=True)
 
     def save(self, *args, **kwargs):
         if self.link:
@@ -21,7 +23,9 @@ class Audio(models.Model):
     title = models.CharField(max_length=255)
     mp3 = models.FileField(upload_to='audio')
     description = models.TextField(default=None, null=True, blank=True)
-
+    creator = models.CharField(max_length=255, default=None, null=True, blank=True)
+    recorded = models.IntegerField(default=None, null=True, blank=True)
+    duration = models.IntegerField(default=None, null=True, blank=True)
 
     def __unicode__(self):
         return '%s' % self.title
